fd80f835da179f9d5871af42e52313cf0c5e6036
Vitaliy <vitasiamen@gmail.com>
2023-04-10 20:22:22 +0300

    create main app with create delivery page

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react';
+import Delivery  from './Delivery';
+
+function App() {
+  const [id, setId] = useState('')
+  const submit = async (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const data = Object.fromEntries(form.entries());
+    const responce = await fetch('http://localhost:8000/deliveries/create',{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        type: "CREATE_DELIVERY",
+        data
+      })
+    });
+    const {id} = await responce.json()
+    setId(id)
+  }
+  return (
+    <div className="py-5">
+      <div className='d-grid gap-2 d-sm-flex justify-content-sm-center mb-5'>
+        {id === '' ? <div className='card'>
+          <div className='card-header'>
+            Create delivery
+          </div>
+          <form className='card-body' onSubmit={submit}>
+            <div className='mb-3'>
+              <input type='number' name='budget' className='form-control' placeholder='Budget'/>
+            </div>
+            <div className='mb-3'>
+              <input type='text' name='notes' className='form-control' placeholder='Notes'/>
+            </div>
+            <button className='btn btn-primary'>Submit</button>
+          </form>
+        </div> : <Delivery/>}
+      </div> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Delivery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Delivery = () => {
+    return (
+        <div>
+            Delivery successfuly created
+        </div>
+    );
+};
+
+export default Delivery
